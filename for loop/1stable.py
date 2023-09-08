85235a3e6ecb1aeb7e67365235de5812b41cc7cb
iamksudharsan <starboysusan@gmail.com>
2023-09-08 12:46:15 +0530

    1'S Tables

---
+++ for loop/1stable.py
@@ -0,0 +1,21 @@
+'''1)
+Print the following using loop
+1*1=1
+1*2=2
+1*3=3
+1*4=4
+1*5=5
+'''
+
+multiply=1   # variable to store the multiplier
+for mul in range(1,6):  # for loop for multiplication of 1
+    print(multiply,"*",mul,"=",multiply*mul) 
+
+'''
+Output:
+1*1=1
+1*2=2
+1*3=3
+1*4=4
+1*5=5
+'''
