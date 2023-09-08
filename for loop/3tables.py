d3ae2ed1a5d2f568582eb7def6d3a379bf6f4b94
iamksudharsan <starboysusan@gmail.com>
2023-09-08 12:47:33 +0530

    1 to 3 tables

---
+++ for loop/3tables.py
@@ -0,0 +1,42 @@
+'''
+2)
+print the following using two loop
+1 * 1 = 1
+1 * 2 = 2
+1 * 3 = 3
+1 * 4 = 4
+1 * 5 = 5
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+'''
+
+for multiplyr in range(1,4):  #Outer loop to change the multiplier
+   for  num in range(1,6): #inner loop change the number to multiper
+        print(multiplyr,"*",num,"=",multiplyr*num)
+'''
+output:
+1 * 1 = 1
+1 * 2 = 2
+1 * 3 = 3
+1 * 4 = 4
+1 * 5 = 5
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+
+'''
